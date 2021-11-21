8ea870941bcefe6279dd584ad0830094d973bf70
iumyx2612 <69593462+iumyx2612@users.noreply.github.com>
2021-11-21 23:08:53 +0700

    Add Detection part

---
+++ Detection/modules.py
@@ -0,0 +1,84 @@
+# general import
+import torch
+import torch.backends.cudnn as cudnn
+
+import os
+
+
+# YOLACT
+# ------------------------------------------------------------
+from yolact.yolact import Yolact
+from yolact.data import set_cfg
+from yolact.utils.functions import SavePath
+
+def config_Yolact(yolact_weight):
+    # Load config from weight
+    model_path = SavePath.from_str(yolact_weight)
+    # TODO: Bad practice? Probably want to do a name lookup instead.
+    config = model_path.model_name + '_config'
+    print('Config not specified. Parsed %s from the file name.\n' % config)
+    set_cfg(config)
+
+    with torch.no_grad():
+        if not os.path.exists('results'):
+            os.makedirs('results')
+
+        # Use cuda
+        use_cuda = True
+        if use_cuda:
+            cudnn.fastest = True
+            torch.set_default_tensor_type('torch.cuda.FloatTensor')
+        else:
+            torch.set_default_tensor_type('torch.FloatTensor')
+
+        # Eval for image, images or video
+        net = Yolact()
+        net.load_weights(yolact_weight)
+        net.eval()
+        print("Done loading YOLACT" + '-'*10)
+        return net
+# ------------------------------------------------------------
+
+
+
+# YOLOv5
+# ------------------------------------------------------------
+from yolov5.models.experimental import attempt_load
+from yolov5.utils_yolov5.general import set_logging, check_img_size
+from yolov5.utils_yolov5.torch_utils import select_device
+
+def config_Yolov5(yolo_weight, imgsz=640):
+    half = False
+    # Initialize
+
+    set_logging()
+    device = select_device('')
+    half &= device.type != 'cpu'  # half precision only supported on CUDA
+
+    # Load model
+    model = attempt_load(yolo_weight, map_location=device)  # load FP32 model
+    stride = int(model.stride.max())  # model stride
+    names = model.module.names if hasattr(model, 'module') else model.names  # get class names
+    imgsz = check_img_size(imgsz, s=stride)
+
+    return model, stride, names, imgsz
+
+# ------------------------------------------------------------
+
+
+
+# Deepsort
+# ------------------------------------------------------------
+from deep_sort.deep_sort_pytorch.utils.parser import get_config
+from deep_sort.deep_sort_pytorch.deep_sort import DeepSort
+
+def config_deepsort(deepsort_cfg):
+# initialize deepsort
+  cfg = get_config()
+  cfg.merge_from_file(deepsort_cfg)
+  deepsort = DeepSort(cfg.DEEPSORT.REID_CKPT,
+                      max_dist=cfg.DEEPSORT.MAX_DIST, min_confidence=cfg.DEEPSORT.MIN_CONFIDENCE,
+                      max_iou_distance=cfg.DEEPSORT.MAX_IOU_DISTANCE,
+                      max_age=cfg.DEEPSORT.MAX_AGE, n_init=cfg.DEEPSORT.N_INIT, nn_budget=cfg.DEEPSORT.NN_BUDGET,
+                      use_cuda=True)
+  return deepsort
---
+++ Detection/yolov5/detect.py
@@ -0,0 +1,224 @@
+# YOLOv5 🚀 by Ultralytics, GPL-3.0 license
+"""
+Run inference on images, videos, directories, streams, etc.
+
+Usage:
+    $ python path/to/detect.py --source path/to/img.jpg --weights yolov5s.pt --img 640
+"""
+
+import argparse
+import sys
+from pathlib import Path
+
+import cv2
+import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  # YOLOv5 root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = ROOT.relative_to(Path.cwd())  # relative
+
+from yolov5.models.experimental import attempt_load
+from yolov5.utils_yolov5.datasets import LoadImages, LoadStreams
+from yolov5.utils_yolov5.general import apply_classifier, check_img_size, check_imshow, check_requirements, check_suffix, colorstr, \
+    increment_path, non_max_suppression, print_args, save_one_box, scale_coords, set_logging, \
+    strip_optimizer, xyxy2xywh
+from yolov5.utils_yolov5.augmentations import Albumentations, augment_hsv, copy_paste, letterbox, mixup, random_perspective
+from yolov5.utils_yolov5.plots import Annotator, colors
+from yolov5.utils_yolov5.torch_utils import load_classifier, select_device, time_sync
+
+train_model_human = '/content/gdrive/MyDrive/Person_retrival_system/Person_retrival_system/yolo5s.pt'
+
+
+@torch.no_grad()
+def run_eval_human(model=None,  # model.pt path(s)
+        img_numpy=None,  # file/dir/URL/glob, 0 for webcam
+        classes=None,  # filter by class: --class 0, or --class 0 2 3
+        imgsz=640,  # inference size (pixels)
+        conf_thres=0.25,  # confidence threshold
+        iou_thres=0.45,  # NMS IOU threshold
+        max_det=1000,  # maximum detections per image
+        device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+        view_img=False,  # show results
+        save_txt=False,  # save results to *.txt
+        save_conf=False,  # save confidences in --save-txt labels
+        save_crop=False,  # save cropped prediction boxes
+        nosave=False,  # do not save images/videos
+        agnostic_nms=False,  # class-agnostic NMS
+        augment=False,  # augmented inference
+        visualize=False,  # visualize features
+        update=False,  # update all models
+        project=ROOT / 'runs/detect',  # save results to project/name
+        name='exp',  # save results to project/name
+        exist_ok=False,  # existing project/name ok, do not increment
+        line_thickness=3,  # bounding box thickness (pixels)
+        hide_labels=False,  # hide labels
+        hide_conf=False,  # hide confidences
+        half=False,  # use FP16 half-precision inference
+        ):
+    stride = int(model.stride.max())
+    names = model.module.names if hasattr(model, 'module') else model.names  # get class names
+
+    """
+    source = str(source)
+    
+    save_img = not nosave and not source.endswith('.txt')  # save inference images
+
+    webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
+        ('rtsp://', 'rtmp://', 'http://', 'https://'))
+
+    # Directories
+    save_dir = increment_path(Path(project) / name, exist_ok=exist_ok)  # increment run
+    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+    
+
+    # Initialize use device
+    set_logging()
+    device = select_device(device)
+    half &= device.type != 'cpu'  # half precision only supported on CUDA
+
+    # Load model
+    w = weights[0] if isinstance(weights, list) else weights
+    classify, suffix, suffixes = False, Path(w).suffix.lower(), ['.pt', '.onnx', '.tflite', '.pb', '']
+    check_suffix(w, suffixes)  # check weights have acceptable suffix
+    pt, onnx, tflite, pb, saved_model = (suffix == x for x in suffixes)  # backend booleans
+    stride, names = 64, [f'class{i}' for i in range(1000)]  # assign defaults
+    if pt:
+        model = attempt_load(weights, map_location=device)  # load FP32 model
+        stride = int(model.stride.max())  # model stride
+        names = model.module.names if hasattr(model, 'module') else model.names  # get class names
+        if half:
+            model.half()  # to FP16
+        if classify:  # second-stage classifier
+            modelc = load_classifier(name='resnet50', n=2)  # initialize
+            modelc.load_state_dict(torch.load('resnet50.pt', map_location=device)['model']).to(device).eval()
+    elif onnx:
+        check_requirements(('onnx', 'onnxruntime'))
+        import onnxruntime
+        session = onnxruntime.InferenceSession(w, None)
+    else:  # TensorFlow models
+        check_requirements(('tensorflow>=2.4.1',))
+        import tensorflow as tf
+        if pb:  # https://www.tensorflow.org/guide/migrate#a_graphpb_or_graphpbtxt
+            def wrap_frozen_graph(gd, inputs, outputs):
+                x = tf.compat.v1.wrap_function(lambda: tf.compat.v1.import_graph_def(gd, name=""), [])  # wrapped import
+                return x.prune(tf.nest.map_structure(x.graph.as_graph_element, inputs),
+                               tf.nest.map_structure(x.graph.as_graph_element, outputs))
+
+            graph_def = tf.Graph().as_graph_def()
+            graph_def.ParseFromString(open(w, 'rb').read())
+            frozen_func = wrap_frozen_graph(gd=graph_def, inputs="x:0", outputs="Identity:0")
+        elif saved_model:
+            model = tf.keras.models.load_model(w)
+        elif tflite:
+            interpreter = tf.lite.Interpreter(model_path=w)  # load TFLite model
+            interpreter.allocate_tensors()  # allocate
+            input_details = interpreter.get_input_details()  # inputs
+            output_details = interpreter.get_output_details()  # outputs
+            int8 = input_details[0]['dtype'] == np.uint8  # is TFLite quantized uint8 model
+    imgsz = check_img_size(imgsz, s=stride)  # check image size
+    """
+    # Dataloader
+
+    # Padded
+    img = letterbox(img_numpy, imgsz, stride=stride, auto=True)[0]
+    # Convert
+    img = img.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
+    img = np.ascontiguousarray(img)
+
+    names = model.module.names if hasattr(model, 'module') else model.names  # get class names
+    vid_path, vid_writer = None, None
+
+    # Run inference
+    model(torch.zeros(1, 3, imgsz, imgsz).to('cuda:0').type_as(next(model.parameters())))  # run once
+    
+    dt, seen = [0.0, 0.0, 0.0], 0
+
+    # Predict
+    t1 = time_sync()
+    img = torch.from_numpy(img).to('cuda:0')
+    img = img.float()
+    img = img / 255.0  # 0 - 255 to 0.0 - 1.0
+    if len(img.shape) == 3:
+      img = img.unsqueeze(0)
+    t2 = time_sync()
+    dt[0] += t2 - t1
+
+    pred = model(img, augment=False, visualize=False)[0]
+        
+    t3 = time_sync()
+    dt[1] += t3 - t2
+
+    # NMS
+    pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+    dt[2] += time_sync() - t3
+
+    # Second-stage classifier (optional)
+    classify = False
+    if classify:
+      pred = apply_classifier(pred, modelc, img, im0s)
+
+    # Process predictions
+    for i, det in enumerate(pred):  # per image
+      seen += 1
+      gn = torch.tensor(img_numpy.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+      imc = img_numpy.copy() if save_crop else img_numpy  # for save_crop
+      annotator = Annotator(img_numpy, line_width=line_thickness, example=str(names))
+
+      # If have detections      
+      if len(det):
+        # Rescale boxes from img_size to im0 size
+        det[:, :4] = scale_coords(img.shape[2:], det[:, :4], img_numpy.shape).round()
+
+        # Print results
+        for c in det[:, -1].unique():
+          n = (det[:, -1] == c).sum()  # detections per class
+      else:
+        return 0
+    # Print results
+    return det[:, :]
+
+
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'yolov5s.pt', help='model path(s)')
+    parser.add_argument('--source', type=str, default=ROOT / 'data/images', help='file/dir/URL/glob, 0 for webcam')
+    parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
+    parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='show results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
+    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --classes 0, or --classes 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--visualize', action='store_true', help='visualize features')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--project', default=ROOT / 'runs/detect', help='save results to project/name')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    parser.add_argument('--line-thickness', default=3, type=int, help='bounding box thickness (pixels)')
+    parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
+    parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
+    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
+    opt = parser.parse_args()
+    opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
+    print_args(FILE.stem, opt)
+    return opt
+
+
+def main(opt):
+    check_requirements(exclude=('tensorboard', 'thop'))
+    run_eval_human(**vars(opt))
+
+
+if __name__ == "__main__":
+    opt = parse_opt()
+    main(opt)
